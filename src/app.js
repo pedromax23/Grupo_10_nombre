a39978c8c680a26db11eba78a2ae53df5a3b1613
pedromax23 <pedromorgade1234@gmail.com>
2023-11-12 11:29:53 -0300

    Inicie el proyecto e instale express

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 const express = require ("express"); 
+const app =express();
+const path = require("path");
 
-const app =express(); 
+app.use(express.static(path.resolve(__dirname, "../public")))
+
+app.listen(3030, () => console.log("Servidor corriendo en el puerto: http://localhost:3030/"))
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./views/index.html")))
