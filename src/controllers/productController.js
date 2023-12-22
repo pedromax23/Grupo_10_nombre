25c9fcd604227014bca6c652506425498da8bfc5
luciano2303 <luciano.otonelli@gmail.com>
2023-12-21 23:30:47 -0300

    Creamos rutas y modificamos el controller para el CRUD de crear y editar

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -37,6 +37,27 @@ const controller = {
         // Renderizo la vista crearProducto
         res.render('products/crearProducto');
     },
+    crearProducto: function(req, res) {
+        // traer const de productos y transformarlo en array
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        // crear nuevo producto y guardarlo en un objeto literal
+        const nuevoProducto = {
+            id: products[products.length - 1].id + 1,
+            nombre: req.body.nombre,
+            img: req.file.filename,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            categoria: req.body.categoria
+        }
+        // hacer el push para agregar productos
+        products.push(nuevoProducto);
+
+        // transformar al JSON
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "), 'utf-8');
+
+        // mostrar la vista al usuario redirigiendo
+        res.redirect('/productos');
+    },
     editar: function(req, res) {
         const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
         // Renderizo la vista editarProducto
@@ -45,7 +66,38 @@ const controller = {
             return producto.id == idProducto
         });
         res.render('products/editarProducto', {productoAEditar});
-    }
+    },
+    editarProducto: function(req, res) {
+        // traer const de productos y transformarlo en array
+        let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+        // filtar por req.params.id con find
+        let idProducto = parseInt(req.params.id); // recuperamos id de la url y lo guardamos en idProducto
+        let productoAEditar = products.find(producto => {
+            return producto.id == idProducto
+        });
+        // sobreescribir el producto en un nuevo objeto literal
+        productoAEditar = {
+            id: productoAEditar.id,
+            nombre: req.body.nombre,
+            img: req.file ? req.file.filename : productoAEditar.img, // if ternario para ver si se subio foto, sino uso la que estaba
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            categoria: req.body.categoria
+        }
+        
+        let indice = products.findIndex(product => {
+            return product.id == idProducto
+        }); // buscamos la posicion del objeto en el array
+
+        products[indice] = productoAEditar;
+
+        // transformar al JSON
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "), 'utf-8');
+
+        // mostrar la vista al usuario redirigiendo
+        res.redirect('/productos/detalle/' + idProducto); // de esta forma redireccionamos a la ruta /productos/detalle / el idProducto que editamos
+    },
 }
 
 //Exportamos controller para requerirlo en productRouter.js
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,25 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
 //Importamos el controlador de Producto
 const productController = require("../controllers/productController");
+
+// Configuración de multer
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/img')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({ storage: storage });
 
 // Ruta pagina productos
 router.get('/', productController.index); //Estaría el listado del producto
 
 // Ruta formulario para crear un producto
 router.get('/crearProducto',productController.crear); 
-// router.post('/crearProducto', productController.crearProducto)
+router.post('/crearProducto', upload.single('imagenCerveza'), productController.crearProducto);
 
 // Ruta formulario para editar un producto
-router.get('/editarProducto/:id?',productController.editar);
-// router.put('/editarProducto', productController.editarProducto)
+router.get('/editarProducto/:id',productController.editar);
+router.put('/editarProducto/:id', upload.single('imagenCerveza'), productController.editarProducto);
 
 // Ruta para ver el carrito de compras
  router.get('/carrito-de-compras', productController.carritoCompras)
 
 // Ruta para ver el detalle de un producto
 router.get('/detalle/:id?', productController.detalleProducto);
+
+// Ruta para eliminar un producto 
+
+// router.delete('/detalle/:id?', productController.eliminarProducto);
 
 
 module.exports = router;
