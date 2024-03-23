1881b83c51878704ecda9eb7e470a15682d3c858
pedromax23 <pedromorgade1234@gmail.com>
2024-03-23 18:33:28 -0300

    Cambios en la validacion del Front del register

--- public/js/register.js
+++ public/js/register.js
@@ -6,52 +6,62 @@ window.addEventListener("load", () => {
 
     form.addEventListener("submit", (event) => {
 
-        // Array de errores
-
         let errorsList = [];
 
-        // NAME
-
+        // NOMBRE
         if(form.name.value == "") {
             errorsList.push("El nombre está vacío");
         } else if (form.name.value.length < 2) {
             errorsList.push("El nombre debe tener al menos 2 caracteres");
         };
 
-        // CONTRASEÑA
+        // APELLIDO
+        if(form.last_name.value == "") {
+            errorsList.push("El Apellido está vacío");
+        } else if (form.name.value.length < 2) {
+            errorsList.push("El Apellido debe tener al menos 2 caracteres");
+        };
 
+        // EMAIL
+        // declaro un regex
+        let emailReg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+        if(form.email.value == "") {
+            errorsList.push("Escribe un email");
+        } else if (!emailReg.test(form.email.value)) {
+            errorsList.push("Debe contener un email valido");
+        }
+        
+        // CONTRASEÑA
         if(form.password.value == "") {
             errorsList.push("La contraseña está vacía");
         } else if (form.password.value.length < 8) {
             errorsList.push("La constraseña debe contener al menos 8 caracteres");
         };
 
-
-        // EMAIL
-
-        // declaro un regex
-
-        let emaillReg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-        if(form.email.value == "") {
-            errorsList.push("Escribe un email");
-        } else if (!emailReg.test(form.email.value)) {
-            errorsList.push("Debe contener un email valido");
-        };
-
         // IMAGEN
-
-        
+        let fileInput = document.getElementById('imagenUsuario');
+        let file = fileInput.files[0];
+        if (!file) {
+            errorsList.push("Por favor, selecciona una imagen.");
+        } else {
+            let allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+            if (!allowedExtensions.test(file.name)) {
+                errorsList.push("La imagen debe tener una extensión válida (jpg, jpeg, png o gif).");
+            } else if (file.size > 10 * 1024 * 1024) { // 10 MB
+                errorsList.push("La imagen es demasiado grande. Por favor, selecciona una imagen más pequeña.");
+            }
+        }
 
         // agregar al listado (inicialmente vacio) los errores como elementos <li>
         // si hay errores, prevengo que se envien
-            if (errorsList.length > 0) {
-                event.preventDefault();
-                errorsHTML.innerHTML = "";
-                errorsList.forEach(error => {
-                    errorsHTML.innerHTML += "<li class='viewErrores'>" + "* " + error + "</li>";
-                });
-            }
+        if (errorsList.length > 0) {
+            event.preventDefault();
+            errorsHTML.innerHTML = "";
+            errorsList.forEach(error => {
+                errorsHTML.innerHTML += "<li class='viewErrores'>" + "* " + error + "</li>";
+            });
+        }
     })
 
     const maxCaracteres = 250;
@@ -59,13 +69,11 @@ window.addEventListener("load", () => {
     const contador = document.querySelector("#contador");
 
     // funcion para actualizar contador de caracteres dispobibles en comentario
-
     function actualizarContador() {
         let caracteresRestantes = maxCaracteres - comentarioInput.value.length;
         contador.textContent = caracteresRestantes;
     };
     comentarioInput.addEventListener("input", actualizarContador);
 
-    // Llamar a actualizarContador para establecer el contador inicial al cargar la página
     actualizarContador();
 });
