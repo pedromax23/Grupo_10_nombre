d995100ff9ea333d969774db9dbd276f0c86e73d
vanesamaria82 <vpaschetta@gmail.com>
2024-03-26 22:46:59 -0300

    Trabajamos en grupo en la actualización de de las bases de datos

--- src/middleware/routes/validacionCategoriaUsuario.js
+++ src/middleware/routes/validacionCategoriaUsuario.js
@@ -1,5 +1,5 @@
 function validarUsuario (req, res, next) {
-    if(req.session.usuarioLogeado.category_id == 3) {
+    if(req.session.usuarioLogeado.category_id == 2) {
         res.redirect('/user/profile')
     }
 
